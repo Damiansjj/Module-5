8d98b7305ca6741232dda028c280007844893ea0
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-18 14:42:59 +0200

    Update H1.mjs

--- H1.mjs
+++ H1.mjs
@@ -1,15 +1,25 @@
 import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
 
-for(let a= 1 ; a < 100; a++){
-    let som =0;
-for(let b = 1; b < a; b++){
-    if(a % b==0){
-        som += b;
-    }
-}
-if(som == a){
-    console.log( a +" is een perfect getal")
-}
+ 
+let GrootGetalReeks;
+perfectNumber(GrootGetalReeks)
+function perfectNumber(GrootGetalReeks)
+{
+    for(let i = 1 ; i < 100; i++)
+    {   let som = 0
+        for(let j = 1; j < i; j++)
+        {
+            if(i % j == 0)
+            {
+                som += j
+            }
+        }
+        if(som == i)
+        {
+            console.log(i + " is een perfect getal");
+        }    
+    }   
+ 
 }
