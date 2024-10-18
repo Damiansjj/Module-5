be9a72cd6e835591d9136e23cd106878febcc27b
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-18 11:10:10 +0200

    Create H1.mjs

---
+++ H1.mjs
@@ -0,0 +1,15 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+for(let a= 1 ; a < 100; a++){
+    let som =0;
+for(let b = 1; b < a; b++){
+    if(a % b==0){
+        som += b;
+    }
+}
+if(som == a){
+    console.log( a +" is een perfect getal")
+}
+}
